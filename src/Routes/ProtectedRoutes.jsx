882f555a7af9c9df01c982272843d0a8d6d76e98
aryan-bhargav <bhargavaryan5@gmail.com>
2025-06-16 14:44:13 +0530

    c

--- src/Routes/ProtectedRoutes.jsx
+++ src/Routes/ProtectedRoutes.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import AlertPopup from "../components/AlertPopup";
+import AlertPopup from "../Components/AlertPopup";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
